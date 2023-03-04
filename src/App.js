3e405ee6d619a948353a24a4ac3bf22598bb5ffd
natekrth <natekrth@gmail.com>
2023-03-05 01:45:05 +0700

    start tailwind and etst

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Home from './pages/Home';
+import './styles/main.css';
 
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl underline text-center text-blue-300 ">
+        Hello world!
+      </h1>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Hello Home</div>
+    <div className='text-blue-600'>Hello Home</div>
   )
 }
 
