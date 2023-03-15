b3ef8825c1475c727b057580cb37436e7a7426a1
natekrth <natekrth@gmail.com>
2023-03-16 00:30:49 +0700

    add artistbox todsob

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Artistbox></Artistbox>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
