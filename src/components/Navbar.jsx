145e63c34cc95c5d4fb88327d219c44e7363d4a7
natekrth <natekrth@gmail.com>
2023-03-15 23:15:04 +0700

    respondsive nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,21 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <nav class="px-2 sm:px-4 py-10">
-        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+      <nav class="px-10 py-10">
+          <ul class="flex flex-row py-4 rounded-lg bg-gray-50 text-sm font-medium">
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+              <a href="#" class="flex-auto py-4 pl-3 pr-4 rounded text-blue-700">Home</a>
             </li>
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Favourite</a>
+              <a href="#" class="flex-auto py-4 pl-3 pr-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white">Favourite</a>
             </li>
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Profile</a>
+              <a href="#" class="flex-auto py-4 pl-3 pr-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white">Profile</a>
             </li>
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</a>
+              <a href="#" class="flex-auto py-4 pl-3 pr-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white">Login</a>
             </li>
           </ul>
-        </div>
       </nav>
     </div>
   )
