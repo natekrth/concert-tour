5888106fece0dea79f12f5bf448c374f7c0b7759
panitnt <panittatanya@gmail.com>
2023-03-16 00:33:17 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import Home from './pages/Home';
 import './main.css';
 import Navbar from './components/Navbar';
 import Artistbox from './components/Artistbox';
+import TourDate from './pages/TourDate';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Artistbox></Artistbox>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/date" element={<TourDate/>} />
       </Routes>
     </div>
   );
---
+++ src/pages/TourDate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TourDate = () => {
+    return (
+        <div className='tour-date-component'>
+            
+        </div>
+    )
+}
+
+export default TourDate
