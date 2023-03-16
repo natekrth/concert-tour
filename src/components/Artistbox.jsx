249d6d909cf06520a0c211c2d0819ccd8e368dfa
natekrth <natekrth@gmail.com>
2023-03-16 17:12:50 +0700

    add cards tailwind

--- src/components/Artistbox.jsx
+++ src/components/Artistbox.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 const Artistbox = () => {
   return (
-    <div className="bg-pink-300 w-full h-40 rounded-lg">
-        <h1 className='text-white'>Date</h1>
+    
+    // <div className="bg-pink-300 w-full h-40 rounded-lg">
+    //     <h1 className='text-white'>Date</h1>
+    // </div>
+    <div>
+        <div class="flex justify-center">
+            <div class="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+                <a href="#!">
+                    <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg" alt="" />
+                </a>
+                <div class="p-6">
+                    <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                        Card title
+                    </h5>
+                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                        TEST
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,32 @@
 import React from 'react'
-// import Navbar from '../components/Navbar'
+// import Navbar from '../components/Navbar'Tk96
 import Artistbox from '../components/Artistbox'
 
 const Home = () => {
   return (
     <div>
-      <h1 className='mt-7'>Upcoming...</h1>
+      <h1 className='text-left mt-7 ml-8 font-semibold'>Upcoming...</h1>
       <div className='grid grid-cols-3 gap-5 m-5'>
         <Artistbox></Artistbox>
       </div>
 
-      <h1 className='mt-7'>Favourite</h1>
+      <h1 className='text-left mt-7 ml-8 font-semibold'>Favourite</h1>
       <div className='grid grid-cols-3 gap-5 m-5'>
         <Artistbox></Artistbox>
       </div>
 
-      <h1 className='mt-7'>Artist</h1>
-      <div className='grid grid-cols-3 gap-5 m-5'>
+      
+      <h1 className='text-left mt-7 ml-8 font-semibold'>Artist</h1>
+      <div className='grid grid-cols-4 gap-5 m-5'>
+        <Artistbox></Artistbox>
+        <Artistbox></Artistbox>
+        <Artistbox></Artistbox>
+        <Artistbox></Artistbox>
+        <Artistbox></Artistbox>
+        <Artistbox></Artistbox>
+        <Artistbox></Artistbox>
+        <Artistbox></Artistbox>
+        <Artistbox></Artistbox>
         <Artistbox></Artistbox>
         <Artistbox></Artistbox>
         <Artistbox></Artistbox>
