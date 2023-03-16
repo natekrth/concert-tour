be81e2c27784de9aaade58af46fc45b1bb6bb298
natekrth <natekrth@gmail.com>
2023-03-16 11:54:59 +0700

    test comment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+// import Navbar from '../components/Navbar'
 import Artistbox from '../components/Artistbox'
 
 const Home = () => {
