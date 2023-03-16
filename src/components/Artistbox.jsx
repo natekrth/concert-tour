1914f5e1ced8234cbce131f47107453bec8ff32a
natekrth <natekrth@gmail.com>
2023-03-16 11:52:23 +0700

    add title for home page

--- src/components/Artistbox.jsx
+++ src/components/Artistbox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Artistbox = () => {
   return (
-    <div className="bg-pink-300 w-full h-32 rounded-lg">
+    <div className="bg-pink-300 w-full h-40 rounded-lg">
         <h1 className='text-white'>Date</h1>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,17 @@ import Artistbox from '../components/Artistbox'
 const Home = () => {
   return (
     <div>
+      <h1 className='mt-7'>Upcoming...</h1>
+      <div className='grid grid-cols-3 gap-5 m-5'>
+        <Artistbox></Artistbox>
+      </div>
+
+      <h1 className='mt-7'>Favourite</h1>
+      <div className='grid grid-cols-3 gap-5 m-5'>
+        <Artistbox></Artistbox>
+      </div>
+
+      <h1 className='mt-7'>Artist</h1>
       <div className='grid grid-cols-3 gap-5 m-5'>
         <Artistbox></Artistbox>
         <Artistbox></Artistbox>
