0dd62449b3e9f05999436c9234f01349d24cc7fb
natekrth <natekrth@gmail.com>
2023-03-18 01:13:33 +0700

    change style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import Navbar from '../components/Navbar'Tk96
 import Artistbox from '../components/Artistbox'
+
 import Upcomingbox from '../components/Upcomingbox'
 
 const Home = () => {
@@ -11,7 +12,9 @@ const Home = () => {
           <h1 className='text-left mt-7 ml-8 font-semibold'>Upcoming...</h1>
           <p className='text-right mt-7 mr-8 font-normal hover:underline ml-8'><a href="url">Show all</a></p>
         </div>
-        <div className='grid grid-cols-2 gap-5 m-5'>
+        <div className='grid grid-cols-3 gap-4 m-5'>
+          <Upcomingbox></Upcomingbox>
+          <Upcomingbox></Upcomingbox>
           <Upcomingbox></Upcomingbox>
         </div>
 
@@ -19,8 +22,10 @@ const Home = () => {
           <h1 className='text-left mt-7 ml-8 font-semibold'>Favourite</h1>
           <p className='text-right mt-7 mr-8 font-normal hover:underline ml-8'><a href="url">Show all</a></p>
         </div>
-        <div className='grid grid-cols-3 gap-5 m-5'>
-          <Artistbox></Artistbox>
+        <div className='grid grid-cols-3 gap-5 m-5 max-[120px]:grid-cols-2'>
+          <Upcomingbox></Upcomingbox>
+          <Upcomingbox></Upcomingbox>
+          <Upcomingbox></Upcomingbox>
         </div>
 
         <div className='grid grid-cols-2'>
@@ -28,7 +33,7 @@ const Home = () => {
           <p className='text-right mt-7 mr-8 font-normal hover:underline ml-8'><a href="url">Show all</a></p>
         </div>
 
-        <div className='grid grid-cols-5 gap-5 m-5'>
+        <div className='grid grid-cols-5 gap-5 m-5 max-md:grid-cols-2'>
           <Artistbox></Artistbox>
           <Artistbox></Artistbox>
           <Artistbox></Artistbox>
