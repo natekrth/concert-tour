40b649dd20aa5a73bda51ebbd6508aade740a045
natekrth <natekrth@gmail.com>
2023-03-17 23:36:05 +0700

    rounded the upcoming

--- src/components/Upcomingbox.jsx
+++ src/components/Upcomingbox.jsx
@@ -4,11 +4,11 @@ const Upcomingbox = () => {
   return (
     <div>
         <div className='flex h-36'>
-            <div class="flex-initial w-32 bg-blue-500">
-                02
+            <div class="flex-initial w-32 bg-blue-500 rounded-l-lg">
+                Photo
             </div>
-            <div class="flex-initial w-64 bg-black">
-                02
+            <div class="flex-initial w-64 bg-blue-800 rounded-r-lg">
+                24 Stamp
             </div>
         </div>
     </div>
