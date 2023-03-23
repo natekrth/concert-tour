e43499cc0bb11006d9d551672bb79950d19e4a5f
natekrth <natekrth@gmail.com>
2023-03-23 21:44:57 +0700

    important adjust to home page margin and everything

--- src/components/Favbox.jsx
+++ src/components/Favbox.jsx
@@ -4,7 +4,7 @@ const Favbox = () => {
   return (
     <div>
         <div className='flex h-36'>
-            <img className="flex-none w-2/5 object-cover rounded-l-lg" src="https://www.myband.co.th/uploads/20181230/94cab1a3d6aed152156b395b0fefd2fc.png" alt="" />
+            <img className="flex-none w-2/5 object-cover rounded-l-lg" src="https://images.workpointnews.com/workpointnews/2022/03/18003821/1647538698_71194_IMG_0767.jpg" alt="" />
             <div className="flex-none w-3/5 bg-white rounded-r-lg grid grid-cols-1">
               <div className='m-auto'>
                 <p className='text-3xl font-semibold text-red-400'>Bowkylion</p>
--- src/components/Upcomingbox.jsx
+++ src/components/Upcomingbox.jsx
@@ -2,17 +2,15 @@ import React from 'react'
 
 const Upcomingbox = () => {
   return (
-    <div>
-        <div className='flex h-36'>
-            <img className="flex-none w-2/5 object-cover rounded-l-lg" src="https://www.daco-thai.com/wp-content/uploads/2021/07/Stamp-01.jpg" alt="" />
-            <div className="flex-none bg-white w-3/5 rounded-r-lg grid grid-cols-1">
-                <h1 className='flex ml-3 mt-2 text-5xl font-semibold text-red-400'>24</h1>
-                <div className='m-auto'>
-                    <p className='text-3xl font-semibold text-red-400'>Stamp</p>
-                    <p className='text-gray-400'>Kasetsart University</p>
-                </div>
-            </div>
-        </div>
+    <div className='flex h-36 w-auto'>
+      <img className="flex-none w-2/5 object-cover rounded-l-lg" src="https://www.daco-thai.com/wp-content/uploads/2021/07/Stamp-01.jpg" alt="" />
+      <div className="flex-none bg-white w-3/5 rounded-r-lg grid grid-cols-1">
+          <h1 className='flex ml-3 mt-2 text-5xl font-semibold text-red-400'>24</h1>
+          <div className='m-auto'>
+              <p className='text-3xl font-semibold text-red-400'>Stamp</p>
+              <p className='text-gray-400'>Kasetsart University</p>
+          </div>
+      </div>
     </div>
   )
 }
