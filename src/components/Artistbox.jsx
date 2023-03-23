3e49f5d66d17745f524438e2db5d470e14a67e7b
natekrth <natekrth@gmail.com>
2023-03-24 00:39:12 +0700

    add fav button

--- src/components/Artistbox.jsx
+++ src/components/Artistbox.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import Like from '../components/Like'
 
 const Artistbox = () => {
   return (
-    
     // <div className="bg-pink-300 w-full h-40 rounded-lg">
     //     <h1 className='text-white'>Date</h1>
     // </div>
     <div>
         <div class="flex justify-center">
-            <div class="block max-w-sm rounded-lg bg-white shadow-lg">
+            <div class="block max-w-sm bg-white rounded-lg shadow-lg">
                 <a href="/">
                     <img class="rounded-t-lg" src="https://www.daco-thai.com/wp-content/uploads/2021/07/Stamp-01.jpg" alt="" />
                 </a>
-                <div class="p-6">
-                    <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800">
+                <div class="p-4 grid grid-cols-2">
+                    <h5 class="mb-2 text-xl font-medium text-neutral-800">
                         Stamp
                     </h5>
+                    <Like></Like>
                     {/* <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
                         @Siam Square
                     </p> */}
