fa3a52f8b41c321fd60da552f28563cdfd2d7634
panitnt <panittatanya@gmail.com>
2023-03-23 23:56:49 +0700

    Done favorite datetour page

--- src/components/DateCard.jsx
+++ src/components/DateCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const DateCard = () => {
+const DateCard = ({img="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"}) => {
     return (
-        <div className='m-4 bg-white border border-gray-200 rounded-lg shadow '>
-            <div className='max-w-md m-auto'>
+    <div className='m-auto my-4 sm:max-w-screen-xs md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl'>
+            <div className='m-4 bg-white border border-gray-200 rounded-lg shadow'>
                 <div className='grid grid-cols-3'>
                     <div className='col-span-1'>
-                        <img class="rounded-lg object-cover h-28 m-auto" src="https://www.daco-thai.com/wp-content/uploads/2021/07/Stamp-01.jpg" alt="" />
+                        <img class="rounded-lg object-cover m-auto sm:h-28 md:h-48 lg:h-48 xl:h-48 2xl:h-48 " src={img} alt="" />
                     </div>
                     <div className='col-span-2 p-1 m-auto'>
                         <h3 className='text-lg'>Stamp</h3>
--- src/pages/TourDate.jsx
+++ src/pages/TourDate.jsx
@@ -6,11 +6,17 @@ const TourDate = () => {
     // useEffect(() => {fetch()})
     return (
         <div className='tour-date-component my-10'>
-            <h1>Your Favorite Artirst Tour</h1>
-            <h2 className='tour-date-head'>Date</h2>
+            <h1 className='text-4xl p-10'>Your Favorite Artirst Tour</h1>
+            <h2 className='text-2xl text-left mx-24'>Date</h2>
+            <DateCard img="https://www.daco-thai.com/wp-content/uploads/2021/07/Stamp-01.jpg"/>
+            <DateCard img='https://www.myband.co.th/uploads/20181230/94cab1a3d6aed152156b395b0fefd2fc.png'/>
             <DateCard/>
+
+            <h2 className='text-2xl text-left mx-24'>Date</h2>
+            <DateCard img="https://www.daco-thai.com/wp-content/uploads/2021/07/Stamp-01.jpg"/>
+            <DateCard img='https://www.myband.co.th/uploads/20181230/94cab1a3d6aed152156b395b0fefd2fc.png'/>
             <DateCard/>
-            <DateCard/>
+
 
         </div>
     )
