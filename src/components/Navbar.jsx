dab87df70ccd4d6d7c9463e3a5bba52724c13c1c
natekrth <natekrth@gmail.com>
2023-03-25 14:03:54 +0700

    add scroll handler

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [lock, setLock] = useState("");
+    const lockHandler = () => {
+      document.body.style.overflow = 'hidden';
+      setLock('hidden');
+    }
   return (
     <div>
-      <nav className="">
+      <nav className="w-full">
           <ul className="flex flex-row py-8 rounded-lg bg-gray-50 text-sm font-medium shadow-md">
             <li>
               <a href="#" className="flex-auto py- pl-32 pr-4 rounded text-blue-700">Home</a>
@@ -15,7 +20,7 @@ const Navbar = () => {
               <a href="#" className="flex-auto py-4 pl-4 pr-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white">Profile</a>
             </li>
             <li>
-              <a href="#" className="flex-auto py-4 pl-4 pr-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white">Login</a>
+              <a href="#" className="flex-auto py-4 pl-4 pr-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white" onClick={lockHandler}>Login</a>
             </li>
           </ul>
       </nav>
