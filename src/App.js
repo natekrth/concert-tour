61e9ca569d8cacfbe886f4d0587c05aaaf17c652
natekrth <natekrth@gmail.com>
2023-03-25 22:58:31 +0700

    move nav bar to app

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Home from './pages/Home';
 import './main.css';
 import Navbar from './components/Navbar';
 import Artistbox from './components/Artistbox';
-
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="w-full h-full text-center">
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
