ea67b2b6d416f63e3c4f44df055cb261e14f81dc
natekrth <natekrth@gmail.com>
2023-03-25 22:58:02 +0700

    adjust navbar alot

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
+import Login from '../components/Login'
 
 const Navbar = () => {
   const [lock, setLock] = useState("");
@@ -10,26 +11,25 @@ const Navbar = () => {
   return (
     <div>
       <nav className="w-full">
-          <ul className="flex flex-row py-8 rounded-lg bg-gray-50 text-sm font-medium shadow-md">
-            <li className='text-center'>
-              <a href="#" className="flex-auto m-auto py-4 pl-32 pr-4 rounded text-blue-700 text">Home</a>
+          <ul className="flex flex-row py-6 rounded-lg bg-gray-50 text-sm font-medium shadow-md">
+            <li className='flex'>
+              <a href="#" className="flex-auto py-2 pl-32 pr-4 rounded text-blue-700">Home</a>
             </li>
-            <li>
-              <a href="#" className="flex-auto py-4 px-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white">Favourite</a>
+            <li className='flex'>
+              <a href="#" className="flex-auto py-2 px-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white">Favourite</a>
             </li>
-            <li>
-              <a href="#" className="flex-auto py-4 px-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white">Profile</a>
+            <li className='flex'>
+              <a href="#" className="flex-auto py-2 pl-4 pr-10 text-gray-700 rounded hover:bg-blue-500 hover:text-white">Profile</a>
             </li>
-            <div className=''>
-              <div className='rounded-full bg-slate-200 flex items-center px-4 w-[200px] sm:w-[400px] lg:w-[500px]'>
-                <AiOutlineSearch size={25} />
-                <input className="bg-transparent p-2 w-full focus:outline-none" type="text" placeholder='Search'></input>
-              </div>
+            <div className='rounded-full bg-slate-200 flex items-center px-4 w-1/2'>
+              <AiOutlineSearch size={25} />
+              <input className="bg-transparent p-2 w-full focus:outline-none" type="text" placeholder='Search'></input>
             </div>
-            <li>
-              <a href="#" className="flex-auto py-4 px-4 text-gray-700 rounded hover:bg-blue-500 hover:text-white" onClick={lockHandler}>Login</a>
+            <li className='flex'>
+              <a href="#" className="flex-auto py-2 pl-24 text-gray-700 rounded hover:bg-blue-500 hover:text-white" onClick={lockHandler}>Login</a>
             </li>
           </ul>
+          {/* <Login isVisable></Login> */}
       </nav>
     </div>
   )
