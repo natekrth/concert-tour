73ff62137247bce52a86dcf0e32942fb28a54938
natekrth <natekrth@gmail.com>
2023-03-25 14:04:24 +0700

    add login modal not finish

---
+++ src/components/Login.jsx
@@ -0,0 +1,18 @@
+import React, { useState }from 'react'
+
+const Login = () => {
+  return (
+    <div className='fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm'>
+        <div className='flex flex-col w-full h-full'>
+            <div className='m-auto'>
+                <div className='bg-slate-100 w-1/2 p-16 rounded'>
+                    <button className='text-black m-auto text-xl place-self-end'>X</button>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sit aliquid asperiores amet in iusto ea modi dicta impedit magnam! Quidem iste rerum animi quasi nobis et quis deleniti qui.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
