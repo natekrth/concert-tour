6ed3b3df7680bf1504d78480dd60493738c852ba
natekrth <natekrth@gmail.com>
2023-03-25 14:13:24 +0700

    adjust close button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,9 +4,9 @@ const Login = () => {
   return (
     <div className='fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm'>
         <div className='flex w-full h-full justify-center items-center'>
-            <div className='bg-slate-100 w-1/2 p-16 rounded'>
-                <button className='text-black m-auto text-xl place-self-end'>X</button>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sit aliquid asperiores amet in iusto ea modi dicta impedit magnam! Quidem iste rerum animi quasi nobis et quis deleniti qui.</p>
+            <div className='bg-slate-100 w-1/2 rounded grid grid-cols-1'>
+                <button className='text-slate-600 font-bold text-xl place-self-start m-10'>X</button>
+                <p className='p-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sit aliquid asperiores amet in iusto ea modi dicta impedit magnam! Quidem iste rerum animi quasi nobis et quis deleniti qui.</p>
             </div>
         </div>
     </div>
